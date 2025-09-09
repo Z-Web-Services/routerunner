bdcdbb9a53f11b0d5d42288ef1d373b55fe12255
Abraham J Bosch <abrahambosch@gmail.com>
2025-09-09 18:52:16 -0500

    cleanup authorizationModes

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -21,13 +21,14 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    //defaultAuthorizationMode: "apiKey",
+    
     defaultAuthorizationMode: "userPool",
 
+    //defaultAuthorizationMode: "apiKey",
     // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
+    // apiKeyAuthorizationMode: {
+    //   expiresInDays: 30,
+    // },
   },
 });
 
